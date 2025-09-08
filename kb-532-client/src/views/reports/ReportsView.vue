142c417da5b076b4dd1bb36c0bfbc45562516a40
chaeun <lce001105@gmail.com>
2025-09-08 15:22:06 +0900

    feat : create section4

--- kb-532-client/src/views/reports/ReportsView.vue
+++ kb-532-client/src/views/reports/ReportsView.vue
@@ -2,11 +2,13 @@
 import MonthlyExpenseDetail from '@/components/reports/section1/MonthlyExpenseDetail.vue';
 import SpendingInsightCard from '@/components/reports/section2/SpendingInsightCard.vue';
 import WeeklySpendingMain from '@/components/reports/section3/WeeklySpendingMain.vue';
+import MonthlySavingsMain from '@/components/reports/section4/MonthlySavingsMain.vue';
 </script>
 <template>
   <div>
     <MonthlyExpenseDetail />
     <SpendingInsightCard />
     <WeeklySpendingMain />
+    <MonthlySavingsMain />
   </div>
 </template>
