9af6acafd6b75627bc99e97e04925b9ae5357553
i-soj-ng <jeong4530@gmail.com>
2025-09-09 20:07:39 +0900

    feat: 지출 내역 페이지 라우팅

--- kb-532-client/src/router/modules/reports.js
+++ kb-532-client/src/router/modules/reports.js
@@ -5,4 +5,10 @@ export default [
     component: () => import('@/views/reports/ReportsView.vue'),
     meta: { bottomNav: true, title: '리포트' },
   },
+  {
+    path: '/expense',
+    name: 'expense',
+    component: () => import('@/views/reports/expense/ExpenseListView.vue'),
+    meta: { bottomNav: true, title: '내 지출 내역'},
+  },
 ];
