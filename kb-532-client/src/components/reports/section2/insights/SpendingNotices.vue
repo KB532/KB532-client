f33289d1ac6437bc043ee2ebeb4f5153b965d840
chaeun <lce001105@gmail.com>
2025-09-10 07:58:15 +0900

    feat : connecting monthly-comparison

--- kb-532-client/src/components/reports/section2/insights/SpendingNotices.vue
+++ kb-532-client/src/components/reports/section2/insights/SpendingNotices.vue
@@ -1,13 +1,20 @@
 <script setup>
 import SummaryChipGroup from '@/components/main/section3/SummaryChipGroup.vue';
-// TODO: 실제 API 데이터로 교체
+
+const props = defineProps({
+  warningText: { type: String, default: '' },
+  recommendText: { type: String, default: '' },
+  warningIcon: { type: String, default: 'bxs:bowl-rice' },
+  recommendIcon: { type: String, default: 'mdi:coffee-outline' },
+  warningLabel: { type: String, default: '경고' },
+  recommendLabel: { type: String, default: '추천' },
+});
+
 const summaryData = {
   categories: {
-    dining: { label: '식비', icon: 'bxs:bowl-rice' },
-    cafe: { label: '카페', icon: 'mdi:coffee-outline' },
+    warning: { label: props.warningLabel, icon: props.warningIcon },
+    recommend: { label: props.recommendLabel, icon: props.recommendIcon },
   },
-  diningRisePercent: 15,
-  cafeTargetPerWeek: 3,
 };
 </script>
 
@@ -16,18 +23,15 @@ const summaryData = {
     class="h-20 rounded-lg bg-gray-100 px-3 py-2 flex flex-col items-center justify-center text-center"
   >
     <SummaryChipGroup :data="summaryData">
-      <!-- warning -->
-      <template #warning="{ data }">
-        <div class="caption2">
-          {{ data.categories.dining.label }}
-          지출이 저번 달보다 {{ data.diningRisePercent }}% 늘었습니다.
+      <template #warning>
+        <div v-if="warningText" class="caption2">
+          {{ warningText }}
         </div>
       </template>
 
-      <!-- recommend -->
-      <template #recommend="{ data }">
-        <div class="caption2">
-          {{ data.categories.cafe.label }} 주문을 주 {{ data.cafeTargetPerWeek }}회로 줄여보세요!
+      <template #recommend>
+        <div v-if="recommendText" class="caption2">
+          {{ recommendText }}
         </div>
       </template>
     </SummaryChipGroup>
