4e207ae7a697045cf271a8cdea5a58652ebf0e1f
i-soj-ng <jeong4530@gmail.com>
2025-09-09 21:01:39 +0900

    feat: 스크롤 오버레이 추가

--- kb-532-client/src/components/reports/expense/section1/UncategorizedList.vue
+++ kb-532-client/src/components/reports/expense/section1/UncategorizedList.vue
@@ -1,6 +1,17 @@
 <script setup>
+import { ref } from "vue";
 import BaseCard from '@/components/common/Card/BaseCard.vue';
 import ExpenseListItem from '@/components/reports/expense/ExpenseListItem.vue';
+import ScrollFadeOverlay from '@/components/common/Overlay/ScrollFadeOverlay.vue';
+
+const listRef = ref(null);
+const isAtBottom = ref(false);
+
+const handleScroll = () => {
+  const el = listRef.value;
+  if (!el) return;
+  isAtBottom.value = el.scrollTop + el.clientHeight >= el.scrollHeight - 1;
+};
 
 const data = [
   {
@@ -35,14 +46,18 @@ const data = [
 </script>
 
 <template>
-  <BaseCard class="flex flex-col gap-4">
+  <BaseCard class="flex flex-col gap-1 relative">
     <div class="flex flex-col gap-1">
       <h1 class="subtitle1">
         <span class="text-red-200">미분류</span>된 지출 내역이 있어요!
       </h1>
       <p class="caption2 text-gray-600">내역을 눌러서 카테고리를 설정해 주세요</p>
     </div>
-    <div class="flex flex-col -mx-4 -mb-4">
+    <div
+      ref="listRef"
+      class="flex flex-col -mx-4 -mb-4 max-h-52 overflow-y-auto"
+      @scroll="handleScroll"
+    >
       <ExpenseListItem
         v-for="item in data"
         :key="item.id"
@@ -52,5 +67,6 @@ const data = [
         :amount="item.amount"
       />
     </div>
+    <ScrollFadeOverlay :isAtBottom="isAtBottom" />
   </BaseCard>
 </template>
