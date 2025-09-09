f4dd7e59ec5c13dbb89d44ed607bb03df18dda99
i-soj-ng <jeong4530@gmail.com>
2025-09-10 00:48:26 +0900

    feat: SlidingModal 공통 컴포넌트 추가

--- kb-532-client/src/components/common/Modal/SlidingModal.vue
+++ kb-532-client/src/components/common/Modal/SlidingModal.vue
@@ -1,12 +1,25 @@
 <script setup>
+import { Icon } from '@iconify/vue';
+
 const props = defineProps({
   modelValue: Boolean,
 });
+
+const emits = defineEmits(['update:modelValue']);
+
+const handleClose = () => {
+  emits('update:modelValue', false);
+};
 </script>
 
 <template>
   <v-bottom-sheet v-model="props.modelValue">
-    <div class="rounded-t-xl bg-white">
+    <div class="rounded-t-xl bg-white p-4 min-h-20">
+      <Icon
+        icon="material-symbols:close-rounded"
+        class="fixed top-4 right-4 size-6 text-gray-600 hover:text-black"
+        @click="handleClose"
+      />
       <slot />
     </div>
   </v-bottom-sheet>
