64084fa26728e52c31c979140acda84e38721d13
i-soj-ng <jeong4530@gmail.com>
2025-09-11 15:56:31 +0900

    feat: 지출 내역 페이지 헤더 수정

--- kb-532-client/src/views/reports/expense/ExpenseListView.vue
+++ kb-532-client/src/views/reports/expense/ExpenseListView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import RevealOnView from '@/components/common/Effects/RevealOnView.vue';
+import TopMenuBar from '@/components/layouts/TopMenuBar.vue'
 import UncategorizedList from '@/components/reports/expense/section1/UncategorizedList.vue';
 import ExpenseList from '@/components/reports/expense/section2/ExpenseList.vue';
 </script>
 
 <template>
+  <TopMenuBar>내 지출 내역</TopMenuBar>
   <RevealOnView :delay="0"><UncategorizedList /></RevealOnView>
   <RevealOnView :delay="100"><ExpenseList /></RevealOnView>
 </template>
