0fa81b53876d38af333375fe65511ef0d51b51b9
chaeun <lce001105@gmail.com>
2025-09-11 11:45:05 +0900

    feat : create dropdown func

--- kb-532-client/src/components/reports/expense/ExpenseDetailModal.vue
+++ kb-532-client/src/components/reports/expense/ExpenseDetailModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { Icon } from '@iconify/vue';
 import SlidingModal from '@/components/common/Modal/SlidingModal.vue';
 import CategoryIcon from '@/components/common/Avatar/IconAvatar.vue';
@@ -8,6 +8,7 @@ import Switch from '@/components/common/Switch/Switch.vue';
 import DropdownItem from '@/components/common/Item/DropdownItem.vue';
 import DropdownModal from '@/components/common/Modal/DropdownModal.vue';
 import { numberWithCommas } from '@/assets/utils/index.js';
+import DarkButton from '@/components/common/Button/DarkButton.vue';
 
 const props = defineProps({
   modelValue: Boolean,
@@ -21,9 +22,38 @@ const handleClose = () => {
 };
 
 const openedDropdown = ref(null);
-
 const toggleDropdown = (key) => {
   openedDropdown.value = openedDropdown.value === key ? null : key;
+};
+
+const selectedCategory = ref('미분류');
+const memoText = ref('');
+const paymentMethod = ref('카드');
+
+watch(
+  () => props.data,
+  (d) => {
+    if (!d) return;
+    selectedCategory.value = d.categoryLabel ?? '미분류';
+    paymentMethod.value = d.methodLabel ?? '카드';
+    memoText.value = d.memo ?? '';
+  },
+  { immediate: true },
+);
+
+const onSelectCategory = (v) => {
+  selectedCategory.value = v;
+  openedDropdown.value = null;
+};
+
+const onConfirmMemo = (v) => {
+  memoText.value = v || '';
+  openedDropdown.value = null;
+};
+
+const onSelectPayment = (v) => {
+  paymentMethod.value = v;
+  openedDropdown.value = null;
 };
 
 const categories = [
@@ -53,9 +83,10 @@ const categories = [
     />
     <div class="flex flex-col gap-4 mt-8">
       <div class="flex gap-2 items-center">
-        <CategoryIcon class="size-6" :category="props.data?.category" />
+        <CategoryIcon class="size-8" :category="props.data?.category" />
         <p class="body1 text-black">{{ props.data?.name }}</p>
       </div>
+
       <div class="flex items-center gap-2">
         <h1 class="title1 text-black">-{{ numberWithCommas(props.data?.amount) }}원</h1>
         <Icon icon="bxs:pencil" class="text-gray-600 size-6 hover:text-black active:text-black" />
@@ -66,20 +97,23 @@ const categories = [
       <div class="relative">
         <div class="flex items-center justify-between body1" @click="toggleDropdown('category')">
           <p class="text-kb-gray-dark">카테고리 설정</p>
-          <DropdownItem title="미분류" :isOpen="openedDropdown === 'category'" />
+          <DropdownItem :title="selectedCategory" :isOpen="openedDropdown === 'category'" />
         </div>
         <div v-if="openedDropdown === 'category'" class="absolute right-0 mt-2 z-50">
-          <DropdownModal :data="categories" />
+          <DropdownModal :data="categories" @select="onSelectCategory" />
         </div>
       </div>
 
       <div class="relative">
         <div class="flex items-center justify-between body1" @click="toggleDropdown('memo')">
           <p class="text-kb-gray-dark">메모</p>
-          <DropdownItem title="메모를 남겨보세요" :isOpen="openedDropdown === 'memo'" />
+          <DropdownItem
+            :title="memoText || '메모를 남겨보세요'"
+            :isOpen="openedDropdown === 'memo'"
+          />
         </div>
         <div v-if="openedDropdown === 'memo'" class="absolute right-0 mt-2 z-50">
-          <DropdownModal type="input" />
+          <DropdownModal type="input" @confirm="onConfirmMemo" />
         </div>
       </div>
 
@@ -93,10 +127,10 @@ const categories = [
       <div class="relative">
         <div class="flex items-center justify-between body1" @click="toggleDropdown('payment')">
           <p class="text-kb-gray-dark">결제 수단</p>
-          <DropdownItem title="카드" :isOpen="openedDropdown === 'payment'" />
+          <DropdownItem :title="paymentMethod" :isOpen="openedDropdown === 'payment'" />
         </div>
         <div v-if="openedDropdown === 'payment'" class="absolute right-0 mt-2 z-50">
-          <DropdownModal :data="['카드', '현금', '계좌이체']" />
+          <DropdownModal :data="['카드', '현금', '계좌이체']" @select="onSelectPayment" />
         </div>
       </div>
 
@@ -105,11 +139,11 @@ const categories = [
         <p>{{ props.data?.date }}</p>
       </div>
 
-      <!--      Todo: 사용처 name이 아니고 데이터에 맞게 바꿔야 함-->
       <div class="flex items-center justify-between body1 text-kb-gray-dark">
         <p>사용처</p>
         <p>{{ props.data?.name }}</p>
       </div>
     </div>
+    <DarkButton block class="mt-4">확인</DarkButton>
   </SlidingModal>
 </template>
